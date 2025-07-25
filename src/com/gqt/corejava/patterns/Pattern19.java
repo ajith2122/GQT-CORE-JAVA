04fd489e06e1d42ae4be5c325c15e1529b481feb
ajith <ajith@AJITHKUMAR.bbrouter>
2025-07-25 21:33:27 +0530

    All loaded pattern programs

--- src/com/gqt/corejava/patterns/Pattern19.java
+++ src/com/gqt/corejava/patterns/Pattern19.java
@@ -2,7 +2,7 @@ package com.gqt.corejava.patterns;
 
 import java.util.Scanner;
 
-public class Project19 {
+public class Pattern19 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ src/com/gqt/corejava/patterns/Pattern21.java
@@ -0,0 +1,25 @@
+package com.gqt.corejava.patterns;
+
+import java.util.Scanner;
+
+public class Pattern21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the count");
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n-i;j++) {
+				System.out.print("-");
+			}
+			for(int j=0;j<=i;j++) {
+				System.out.print("#");
+			}
+			System.out.println();
+			sc.close();
+		}
+	  }
+	}
+
+
