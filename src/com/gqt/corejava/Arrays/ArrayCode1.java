d906a9b1bf0ee256e5f513153b81a2c4e86e243d
ajith <ajith@AJITHKUMAR>
2025-07-29 16:04:09 +0530

    all loaded arraycode porgrams

---
+++ src/com/gqt/corejava/Arrays/ArrayCode1.java
@@ -0,0 +1,29 @@
+package com.gqt.corejava.Arrays;
+
+import java.util.Scanner;
+
+public class ArrayCode1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the student count");
+		int n = sc.nextInt();
+		//Creating the array to store n students marks
+		int arr[] = new int[n];
+		//Collecting & Storing marks of n students
+		for (int i =0;i<arr.length;i++)
+		{
+			System.out.println("Enter the marks of students no: "+(i+1));
+			arr[i] = sc.nextInt();
+		}
+		System.out.println("-------");
+		//Display marks stored inside the array 
+		for (int i=0;i<arr.length;i++)
+		{
+			System.out.println("The marks of student no: "+(i+1) +"is="+arr[i]);
+		}
+        sc.close();
+	}
+
+}
---
+++ src/com/gqt/corejava/Arrays/ArrayCode2.java
@@ -0,0 +1,46 @@
+package com.gqt.corejava.Arrays;
+
+import java.util.Scanner;
+
+class ArrayOperations
+       {
+    	        int arr[];
+    	        Scanner sc=new Scanner(System.in);
+    	        void createArray() 
+    	        {
+    	        	System.out.println("Enter the student count");
+    	        	int n=sc.nextInt();
+    	        	arr =new int[n];
+    	        	System.out.println("Array created");
+    	        	System.out.println("-------");
+    	        }
+    	        void collectData()
+    	        {
+    	        	for (int i =0;i<arr.length;i++)
+    	    		{
+    	        		System.out.println("Enter the marks of students no: "+(i+1));
+    	    			arr[i] = sc.nextInt();
+    	    		}
+    	        	System.out.println("Data stored into the array");
+    	        }
+    	        void displayData() {
+    	        	for (int i =0;i<arr.length;i++)
+    	    		{
+    	        		System.out.println("Enter the marks of students no: "+(i+1)+"is="+arr[i]);
+    	    		}
+    	      }
+       }
+      
+public class ArrayCode2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayOperations ao = new ArrayOperations();
+		ao.createArray();
+		ao.collectData();
+		ao.displayData();
+		
+
+	}
+
+}
